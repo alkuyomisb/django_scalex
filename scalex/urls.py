c6e9415ea95046c58908556780a61fd61e06edff
shrooq <shrooq.alhashli@swiftbeam.co>
2022-12-19 14:49:04 +0400

    add result page

--- scalex/urls.py
+++ scalex/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('filter', views.filter, name='filter'),
+    path('result', views.result, name='result'),
     path('admin/', admin.site.urls),
 ]
--- scalex/views.py
+++ scalex/views.py
@@ -64,6 +64,23 @@ def filter(request):
 
 
 
-# 
+def result(request):
+    # export_packages()
+    data2 = {
+        "data_allowance": prepaid.data_allowance ,
+        "prices": prepaid.prices ,
+        "times": prepaid.times ,
+        "social_media_data": prepaid.social_media_data ,
+        "flexi_minutes": prepaid.flexi_minutes ,
+        "categories": prepaid.categories ,
+        "titles": baqati.titles ,
+        "prices2": baqati.prices ,
+        "times2": baqati.times ,
+        "flexi_minutes2": baqati.flexi_minutes ,
+    }
+    return render(request, "bestplan/result.html" , data2)
 
+
+
+    
 
