68babbc48eb52635c9f128ece8291797f51bec1a
shrooq <shrooq.alhashli@swiftbeam.co>
2023-01-18 13:37:01 +0400

    get price, GB, titles

--- scalex/isp/vodafone/mobilePlans.py
+++ scalex/isp/vodafone/mobilePlans.py
@@ -11,173 +11,113 @@ class vodafonePlans:
 
     def __init__(self):
         self.reset()
-        self.get_our_plans_block1()
-        self.get_our_plans_block2()
-        self.get_our_plans_block3()
-        self.get_our_plans_block4()
-        self.get_our_plans_block5()
-        self.get_our_plans_block6()
+        self.get_our_plans_blocks()
 
-    def get_our_plans_block1(self):
+
+    def get_our_plans_blocks(self):
         divs_tags = self.soup.find_all("div")
         for div in divs_tags:
             try:
                 if div['class'][0] == "plan__wrapper":
-                    span_tags = div.find_all("span")
-                    our_plan_block1 = {}
-                    for span in span_tags:
-                        if '35' in span.text:
-                            our_plan_block1["GB"] = span.text
-                        if '16' in span.text and 'OMR' in span.text:
-                            our_plan_block1["price"] = span.text.replace("\n" , " ")
-                if div['class'][0] == "plan__middle":
-                    if "Vodafone" in div.text and "Elite" in div.text:
-                        our_plan_block1["title"] = div.text
-                if div['class'][0] == "plan__title":
-                    if "20" in div.text and "Local Data" in div.text:
-                        our_plan_block1["local_data"] = div.text
-                    if "5 GB" in div.text and "Entertainment Pass" in div.text:
-                        our_plan_block1["entertainment_pass"] = div.text
-                    if "60" in div.text :
-                        our_plan_block1["international_minutes"] = div.text
-            except:
-                pass
-    
-    def get_our_plans_block2(self):
-        divs_tags = self.soup.find_all("div")
-        for div in divs_tags:
-            try:
-                if div['class'][0] == "plan__wrapper":
-                    span_tags = div.find_all("span")
-                    our_plan_block2 = {}
-                    for span in span_tags:
-                        if "80" in span.text:
-                            our_plan_block2["GB"] = span.text
-                        if "24" in span.text and "OMR" in span.text:
-                            our_plan_block2["price"] = span.text.strip().replace("\n" , " ")
-                if div['class'][0] == "plan__middle":
-                    if "RED Pioneer" in div.text:
-                        our_plan_block2["title"] = div.text
-                if div['class'][0] == "plan__title":
-                    if "40 GB" in div.text:
-                        our_plan_block2["local_data"] = div.text
-                    if "20 GB" in div.text and "Social Pass" in div.text:
-                        our_plan_block2["social_pass"] = div.text
-                    if "20 GB" in div.text and "Entertainment Pass" in div.text:
-                        our_plan_block2["entertainment_pass"] = div.text
-                    if "200" in div.text:
-                        our_plan_block2["international_minutes"] = div.text
-            except:
-                pass
+                    our_plan_block = {}
+                    
+                    title = div.find_all("div" , {"class": "plan__middle"})[0]
+                    titles_fileds = div.find_all("div" , {"class": "plan__title"})
+                    sub_titles_fileds = div.find_all("div" , {"class" : "plan__subtitle"})
+                    price = div.find_all("span" , {"class" : "plan__price"})[0]
+                    gb = div.find_all("span")[0]
 
-    def get_our_plans_block3(self):
-        divs_tags = self.soup.find_all("div")
-        for div in divs_tags:
-            try:
-                if div['class'][0] == "plan__wrapper":
-                    span_tags = div.find_all("span")
-                    our_plan_block3 = {}
-                if div['class'][0] == "plan__data":
-                    for span in span_tags:
-                        if "25" in span.text:
-                            our_plan_block3["GB"] = span.text
-                        if "12" in span.text and "OMR" in span.text:
-                            our_plan_block3["price"] = span.text.strip().replace("\n" , " ")
-                if div['class'][0] == "plan__middle":
-                    if "RED Explore" in div.text:
-                        our_plan_block3["title"] = div.text
-                if div['class'][0] == "plan__title":
-                    if "15 GB" in div.text:
-                        our_plan_block3["local_data"] = div.text
-                    if "10 GB Social Pass" in div.text:
-                        our_plan_block3["social_pass"] = div.text
-                    if "500" in div.text:
-                        our_plan_block3["calling_minutes"] = div.text
-                        # print(our_plan_block3)
-            except:
-                pass
 
-    def get_our_plans_block4(self):
-        divs_tags = self.soup.find_all("div")
-        for div in divs_tags:
-            try:
-                if div['class'][0] == "plan__wrapper":
-                    span_tags = div.find_all("span")
-                    our_plan_block4 = {}
-                if div['class'][0] == "plan__data":
-                    for span in span_tags:
-                        if span.text.startswith(('5GB')):
-                            our_plan_block4["GB"] = span.text
-                if div['class'][0] == "plan__middle":
-                    if "RED Advance" in div.text:
-                        our_plan_block4["title"] = div.text
-                if div['class'][0] == "plan__title":
-                    if div.text.startswith('5') and "Local Data" in div.text:
-                        our_plan_block4["Local_data"] = div.text
-                    if "150" in div.text and "Calling Minutes" in div.text:
-                        our_plan_block4["calling_minutes"] = div.text
-            except:
-                pass
+                    our_plan_block["GB"] = gb.text
+                    our_plan_block["price"] = price.text.replace("\n" , " ").strip()
+                    our_plan_block["title"] = title.text
 
-    def get_our_plans_block5(self):
-        divs_tags = self.soup.find_all("div")
-        for div in divs_tags:
-            try:
-                if div['class'][0] == "plan__wrapper":
-                    span_tags = div.find_all("span")
-                    our_plan_block5 = {}
-                if div['class'][0] == "plan__data":
-                    for span in span_tags:
-                        if "2GB" in span.text:
-                            our_plan_block5["GB"] = span.text
-                        # if "2" in span.text and "OMR" in span.text:
-                        #     print(span.text)
-                if div['class'][0] == "plan__middle":
-                    if "Start" in div.text:
-                        our_plan_block5["title"] = div.text
-                if div['class'][0] == "plan__title":
-                    if "2 GB" in div.text:
-                        our_plan_block5["local_data"] = div.text
-                    if "20" in div.text and "Calling Minutes" in div.text:
-                        our_plan_block5["calling_minutes"] = div.text
-                    if "7 Days" in div.text:
-                        our_plan_block5["days"] = div.text
-                if div['class'][0] == "plan__subtitle":
-                    if "Valid for 7 days" in div.text:
-                        our_plan_block5["days_sub_title"] = div.text
-            except:
-                pass
-    
+                    for filed in titles_fileds:
+                        if 'Local Data' in filed.text:
+                            our_plan_block["local_data"] = filed.text
+                        if 'Calling' in filed.text:
+                            our_plan_block["calling_minutes"] = filed.text
+                        if 'Social Pass' in filed.text:
+                            our_plan_block["social_pass"] = filed.text
+                        if 'Entertainment' in filed.text:
+                            our_plan_block["entertainment_pass"] = filed.text
+                        if 'Unlimited' in filed.text:
+                            our_plan_block["minutes_sms"] = filed.text
+                        if 'Take' in filed.text:
+                            our_plan_block["home_tariff"] = filed.text
+                        if 'International' in filed.text:
+                            our_plan_block["international_minutes"] = filed.text
+                        if 'weeks' in filed.text:
+                            our_plan_block["weeks"] = filed.text
+                        if 'VAT' in filed.text:
+                            our_plan_block["vat"] = filed.text
+                        if 'Great' in filed.text:
+                            our_plan_block["add-ons"] = filed.text
+                            
+                    self.mobile_plans_blocks.append(our_plan_block)
 
-    def get_our_plans_block6(self):
-        divs_tags = self.soup.find_all("div")
-        for div in divs_tags:
-            try:
-                if div['class'][0] == "plan__wrapper":
-                    span_tags = div.find_all("span")
-                    our_plan_block6 = {}
-                if div['class'][0] == "plan__data":
-                    for span in span_tags:
-                        # list
-                        # if "15GB" in span.text:
-                        #     print(span)
-                        if "8" in span.text and "OMR" in span.text:
-                            print(span.text)
-                if div['class'][0] == "plan__middle" :
-                    if "RED Premium" in div.text:
-                        our_plan_block6["title"] = div.text
-                if div['class'][0] == "plan__title":
-                    if "10 GB" in div.text and "Local Data" in div.text:
-                        our_plan_block6["local_data"] = div.text
-                    if "5 GB" in div.text and  "Social Pass" in div.text:
-                        our_plan_block6["social_pass"] = div.text
-                    if "300" in div.text:
-                        our_plan_block6["calling_minutes"] = div.text
+                    print(self.mobile_plans_blocks)
+                    
+                   
+                   
+                #     for span in span_tags:
+                #         if '35' in span.text:
+                #             our_plan_block1["GB"] = span.text
+                #         if '16' in span.text and 'OMR' in span.text:
+                #             our_plan_block1["price"] = span.text.replace("\n" , " ")
+               
+               
+               
+               
+               
+               
+                # if div['class'][0] == "plan__middle":
+                #     if "Vodafone" in div.text and "Elite" in div.text:
+                #         our_plan_block1["title"] = div.text
+                # if div['class'][0] == "plan__title":
+                #     if "20" in div.text and "Local Data" in div.text:
+                #         our_plan_block1["local_data"] = div.text
+                #     # if "10 GB" in div.text and "Social Pass" in div.text:
+                #     #     print(div.text)
+                #     if "5 GB" in div.text and "Entertainment Pass" in div.text:
+                #         our_plan_block1["entertainment_pass"] = div.text
+                #     # if "Unlimited" in div.text:
+                #     #     print(div.text)
+                #     # if "Take your" in div.text:
+                #     #     print(div.text)
+                #     if "60" in div.text :
+                #         our_plan_block1["international_minutes"] = div.text
+                #         # print(our_plan_block1)
+                #         # print(our_plan_block1)
+                #     # if "4 weeks" in div.text:
+                #     #     print(div.text)
+                #     # if "VAT" in div.text:
+                #     #     print(div.text)
+                        
+                        
+                # if div['class'][0] == "plan__subtitle":
+                    # if "Surf" in div .text:
+                    #     print(div.text)
+                    # if "Enjoy" in div.text:
+                    #     print(div.text)
+                    # if "Stream" in div.text:
+                    #     print(div.text)
+                    # if "Call and text" in div.text:
+                    #     print(div.text)
+                    # if "Use your local data," in div.text:
+                    #     print(div.text)
+                    # if "Keep in touch" in div.text:
+                    #     print(div.text)
+                    # if "Valid for 4 weeks" in div.text:
+                    #     print(div.text)
+                    # if "All prices" in div.text:
+                    #     print(div.text)
+                    
 
 
             except:
                 pass
-
+   
 
                 
     def reset(self):
